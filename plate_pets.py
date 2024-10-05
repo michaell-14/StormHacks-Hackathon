5b04122559309294376b5d16298a5a4ca1ddf083
kayla-kjl <kaylajeehaelee05@gmail.com>
2024-10-05 14:33:49 -0700

    add title

--- plate_pets.py
+++ plate_pets.py
@@ -3,22 +3,31 @@ from sys import exit
 
 pygame.init()
 
+#initial info and set up
 width= 1000
 height= 500
 screen = pygame.display.set_mode((width,height)) #frame size
 pygame.display.set_caption('kaykaygamegame')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('animations/Pixeltype.ttf', 70)
 
 
+title_surf = test_font.render('special kaykay game game', False, 'Black' )
+title_rect = title_surf.get_rect(midtop = (width/2,50))
+
+#making grount surface
 ground_surf = pygame.image.load('animations/ground.png').convert_alpha()
 ground_surf = pygame.transform.scale(ground_surf, (width, ground_surf.get_height() * width // ground_surf.get_width()))
 ground_rect = ground_surf.get_rect(midbottom = (width/2,height))
+#top of grass is around 320ish
 
 
-
+#making sky surfce
 sky_surf = pygame.image.load('animations/sky.png').convert_alpha()
 sky_surf = pygame.transform.scale(sky_surf, (width, sky_surf.get_height() * width // sky_surf.get_width()))
 sky_rect = sky_surf.get_rect(midtop = (width/2,0))
+
+
 
 
 while True:
@@ -27,13 +36,16 @@ while True:
             pygame.quit() #quits
             exit()
 
+
+
     screen.blit(sky_surf, sky_rect)
     screen.blit(ground_surf,ground_rect)
-    
+    screen.blit(title_surf,title_rect)
     
 
 
     
+
 
     pygame.display.update() #updates the display surface
     clock.tick(60) #while look shouldnt run faster then 60x per second
