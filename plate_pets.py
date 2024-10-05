f809328991d8a7fe34185e26150c0ae108c73200
kayla-kjl <kaylajeehaelee05@gmail.com>
2024-10-05 14:19:38 -0700

    sky

--- plate_pets.py
+++ plate_pets.py
@@ -3,20 +3,22 @@ from sys import exit
 
 pygame.init()
 
-width= 800
-height= 400
+width= 1000
+height= 500
 screen = pygame.display.set_mode((width,height)) #frame size
 pygame.display.set_caption('kaykaygamegame')
 clock = pygame.time.Clock()
 
 
 ground_surf = pygame.image.load('animations/ground.png').convert_alpha()
-ground_surf = pygame.transform.scale(ground_surf, (800, ground_surf.get_height() * 800 // ground_surf.get_width()))
+ground_surf = pygame.transform.scale(ground_surf, (width, ground_surf.get_height() * width // ground_surf.get_width()))
+ground_rect = ground_surf.get_rect(midbottom = (width/2,height))
 
-ground_rect = ground_surf.get_rect(center = (400,200))
 
 
-#sky_surf
+sky_surf = pygame.image.load('animations/sky.png').convert_alpha()
+sky_surf = pygame.transform.scale(sky_surf, (width, sky_surf.get_height() * width // sky_surf.get_width()))
+sky_rect = sky_surf.get_rect(midtop = (width/2,0))
 
 
 while True:
@@ -25,7 +27,9 @@ while True:
             pygame.quit() #quits
             exit()
 
+    screen.blit(sky_surf, sky_rect)
     screen.blit(ground_surf,ground_rect)
+    
     
 
 
