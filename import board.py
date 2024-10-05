6f2cd664cc4f894fac90893b28de02ba0fb8babc
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-05 12:45:10 -0700

    board code

---
+++ import board.py
@@ -0,0 +1,17 @@
+import board
+import busio
+from digitalio import DigitalInOut
+from adafruit_pn532.i2c import PN532_I2C
+
+i2C = busio.I2C(board.SCL. board.SDA)
+pn532 = PN532_I2C(i2c, debug=False)
+
+pn532.SAM_configuration()
+
+print("Waiting for RFID/NFC card...")
+while True:
+    uid = pn532.read_passive_target(timeout=100)
+    if uid is None:
+        continue
+    print("Found card with UID:", [hex(i) for i in uid]) # Print UID of card
+
