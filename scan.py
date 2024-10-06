faee2fe989cc5d2bf967727f772c29fa409602c3
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-05 17:04:32 -0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -9,23 +9,22 @@ pn532 = PN532_I2C(i2c, debug=False)
 pn532.SAM_configuration()
 
 print("Waiting for an NFC card...")
+tag1 = None
+tag2 = None
+
 while True:
     print("Waiting for an NFC card..." + "\n")
     uid = pn532.read_passive_target() #attempts to read a card
-    print([hex(i) for i in uid])
-    if uid is not None:
-        print("Found card with UID:", [hex(i) for i in uid])
-        tag1 = [hex(i) for i in uid]
-        print("tag1: ", tag1)
-        continue
-    time.sleep(.25)
-    if uid is tag1:
+    if tag1 is None:
+            tag1 = [hex(i) for i in uid]
+            print("tag1: ", tag1)
+    elif [hex(i) for i in uid] == tag1:
         print("Same card")
-        continue
-    if uid is not tag1: 
+    else:
         tag2 = [hex(i) for i in uid]
         print("tag2: ", tag2)
         break
+    time.sleep(0.25)
 if tag1 and tag2 != None:
     print("Both tags found")
 
