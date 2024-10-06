a721f5bfa34afcdf1d0facd0487216a77104717b
victor-t-2702 <victor.t.2702@gmail.com>
2024-10-05 20:09:12 -0700

    Update TESTtagStuff.py

--- TESTtagStuff.py
+++ TESTtagStuff.py
@@ -2,7 +2,7 @@ import tagStuff
 import time
 
 animalRead = tagStuff.readTag() #call Victor's readTag function to get the value associated with the tag's UID
-time.sleep(1)
+time.sleep(5)
 foodRead = tagStuff.readTag() #call Victor's readTag function to get the value associated with the tag's UID
 checkMatch = tagStuff.checkMatch(animalRead, foodRead) #call Victor's checkMatch function to check if the animal and food match
 
