47a78525d9bb294d1323ae89cafc4bae25621053
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-05 22:39:50 -0700

    Update scan.py
    adding threading lock

--- scan.py
+++ scan.py
@@ -3,28 +3,42 @@ from adafruit_pn532.i2c import PN532_I2C
 import time
 import board
 import busio
+import threading
 
 i2c = busio.I2C(board.SCL, board.SDA)
 pn532 = PN532_I2C(i2c, debug=False)
 pn532.SAM_configuration()
 
 tags = []
-def scan_multi():
+lock = threading.Lock()
+
+def scan_tag():
     f = 0
     while f == 0:
+        
         uid = pn532.read_passive_target()  # attempts to read a card
         if uid is not None:
             tag = [hex(i) for i in uid]
-            if tag not in tags:
-                tags.append(tag)
-                print("New tag found: ", tag)
-        else:
-            print("Same card")
+            with lock:
+                if tag not in tags:
+                    tags.append(tag)
+                    print("New tag found: ", tag)
+                if tags.__len__() == 2:
+                    print("Two tags found")
+                    time.sleep(0.05)
+                f=1
+                break
+        else: 
+            continue
 
-        if tags.__len__() == 2:
-            f=1
-            print("Two tags found")
-        time.sleep(0.05)
+def scan_multi(): 
+    threads = []
+    for i in range(2):
+        t = threading.Thread(target=scan_tag)
+        threads.append(t)
+        t.start()
 
-
+    for t in threads:
+        t.join()
+        
 scan_multi()
