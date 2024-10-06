cbfb3495d74a7f16e80198ab52a9236356298054
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-05 21:46:03 -0700

    Update scan.py
    created function for modular

--- scan.py
+++ scan.py
@@ -10,19 +10,19 @@ pn532.SAM_configuration()
 
 tags = []
 f = 0
-
-while f == 0:
-    uid = pn532.read_passive_target()  # attempts to read a card
-    if uid is not None:
-        tag = [hex(i) for i in uid]
-        if tag not in tags:
-            tags.append(tag)
-            print("New tag found: ", tag)
+def scan_multi():
+    while f == 0:
+        uid = pn532.read_passive_target()  # attempts to read a card
+        if uid is not None:
+            tag = [hex(i) for i in uid]
+            if tag not in tags:
+                tags.append(tag)
+                print("New tag found: ", tag)
         else:
             print("Same card")
 
         if tags.__len__() == 2:
             f=1
             print("Two tags found")
-    time.sleep(0.05)
+        time.sleep(0.05)
 
