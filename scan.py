3e693c9c8caebea8a724f85a133909cb657a6134
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2024-10-05 17:47:42 -0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,29 +1,28 @@
-import board
-import busio
 from digitalio import DigitalInOut
 from adafruit_pn532.i2c import PN532_I2C
 import time
+import board
+import busio
 
 i2c = busio.I2C(board.SCL, board.SDA)
 pn532 = PN532_I2C(i2c, debug=False)
 pn532.SAM_configuration()
 
-tag1 = hex(0)
-tag2 = hex(0)
+tags = []
 f = 0
 
 while f == 0:
     print("Waiting for an NFC card..." + "\n")
-    uid = pn532.read_passive_target() #attempts to read a card
-    if tag1 is hex(0):
-        tag1 = [hex(i) for i in uid]
-        print("tag1: ", tag1)
-    elif [hex(i) for i in uid] == tag1:
-        print("Same card")
-    else:
-        tag2 = [hex(i) for i in uid]
-        print("tag2: ", tag2)
-        f = 1
-if tag1 and tag2 != None:
-    print("Both tags found")
+    uid = pn532.read_passive_target()  # attempts to read a card
+    if uid is not None:
+        tag = [hex(i) for i in uid]
+        if tag not in tags:
+            tags.append(tag)
+            print("New tag found: ", tag)
+        else:
+            print("Same card")
+
+        if tags.__len__() == 2:
+            f=1
+            print("Two tags found")
 
